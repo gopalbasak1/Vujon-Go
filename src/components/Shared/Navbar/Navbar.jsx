75ee865c501f624f0e623bc568e474d544fa6f81
gopalbasak1 <gopalbasak1223@gmail.com>
2024-03-31 19:34:19 +0600

    completed navbar

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,15 +1,42 @@
+import { useState } from 'react';
 import { assets } from '../../../assets/assets';
 import './Navbar.css'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <div className='navbar w-[5px] h-[50px]'>
-            <img className='logoname' src={assets.vujon} alt="" />
 
+    const [menu, setMenu] = useState('Menu');
+
+    return (
+        <div className='navbar'>
+            
+            <img className='logoName' src={assets.vujon} alt="" />
+           
         <ul className="navbar-menu">
 
+            <li onClick={()=> setMenu('Home')} className={menu === 'Home' ? 'active' : ''}>Home</li>
+            <li onClick={()=> setMenu('Menu')} className={menu === 'Menu' ? 'active' : ''}>Menu</li>
+            <li onClick={()=> setMenu('Mobile-App')} className={menu ===  'Mobile-App' ? 'active' : ''}>Mobile-App</li>
+            <li onClick={()=> setMenu('Contact-Us')} className={menu === 'Contact-Us' ? 'active' : ''}>Contact Us</li>
+
         </ul>
-            <li></li>
+
+        <div className="navbar-right">
+            <img src={assets.search_icon} alt="" />
+
+            <div className="navbar-search-icon">
+                <img src={assets.basket_icon} alt="" />
+
+                <div className="dot"></div>
+
+            </div>
+
+            
+
+            <button>Sign In</button>
+
+        </div>
+
         </div>
     );
 };
