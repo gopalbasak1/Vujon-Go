8ef8e2d2ab34427055c0dad24b0f8269441004bb
gopalbasak1 <gopalbasak1223@gmail.com>
2024-04-02 13:49:44 +0600

    add explore menu section

---
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,35 @@
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets';
+
+
+const ExploreMenu = ({category, setCategory}) => {
+
+    return (
+        <div className='explore-menu' id='explore-menu'>
+
+            <h1>Explore our menu</h1>
+
+            <p className='explore-menu-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt laudantium itaque at saepe commodi fuga, cupiditate, similique amet quaerat aspernatur earum provident illo temporibus voluptates numquam dolore dolorem deserunt placeat.</p>
+
+            <div className="explore-menu-list">
+                {
+                    menu_list.map((item,index)=>{
+                        return(
+                            <div
+                             onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} 
+                             key={index} className="explore-menu-list-item">
+                                <img className={category===item.menu_name?"active": ""} src={item.menu_image} alt="" />
+                                <p>{item.menu_name}</p>
+                            </div>
+                        )
+                    })
+                }
+                <hr />
+            </div>
+
+            
+        </div>
+    );
+};
+
+export default ExploreMenu;
